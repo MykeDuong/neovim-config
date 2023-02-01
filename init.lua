eee6f1ddf48678930a056bad2fbcc5c95d6a3898
Minh Duong <hongminh4402@gmail.com>
2023-01-31 19:39:28 -0700

    Stuff

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 require("packer-require")
 require("remap")
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
--- lua/packer-require/init.lua
+++ lua/packer-require/init.lua
@@ -8,4 +8,20 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+  use({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+        require("rose-pine").setup()
+        vim.cmd('colorscheme rose-pine')
+    end
+  })
+
+  use( 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'} )
+  use( 'nvim-treesitter/playground' )
+  use( 'nvim-lua/plenary.nvim' )
+  use( 'ThePrimeagen/harpoon' )
+  use( 'mbbill/undotree' )
+  use( 'tpope/vim-fugitive' )
 end)
