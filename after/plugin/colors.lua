704250eb52f2e8e3433e6592ac852297650f259e
mykeduong <hongminh4402@gmail.com>
2023-12-18 00:43:28 -0700

    transparent

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,6 +4,8 @@ function ColorVim(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+	vim.api.nvim_set_hl(0, "NormalSB", { bg = "none"})
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none"})
 end
 
 ColorVim()
